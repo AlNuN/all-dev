1008a16606bff17da66dab2f08988ed428afc6ee
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-05-25 19:02:25 -0300

    Import font at _document

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-@import url(${`https://fonts.googleapis.com/css2?family=Inter&display=swap`});
-
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
