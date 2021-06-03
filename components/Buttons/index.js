3fb0f099d9089cf526b5c8ab500256f3125ee3b8
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-06-03 20:31:05 -0300

    fix pseudo link order

--- components/Buttons/index.js
+++ components/Buttons/index.js
@@ -11,33 +11,36 @@ const ButtonOut = styled(Button)`
   background: ${({ theme }) => theme.colors.outlined};
   color: ${({ theme }) => theme.colors.white};
 
-  &:hover, &:active {
-    background: ${({ theme }) => theme.colors.btnOutlinedHover} !important;
-  }
-
-  &:active {
-    border: 4px solid #173569;
+  &:hover {
+    background: ${({ theme }) => theme.colors.btnOutlinedHover};
   }
 
   &:focus {
     background: #173569;
   }
+
+  &:active {
+    border: 4px solid #173569;
+    background: ${({ theme }) => theme.colors.btnOutlinedHover};
+  }
+
 `
 
 const ButtonFill = styled(Button)`
   background: ${({ theme }) => theme.colors.blue300};
   color: ${({ theme }) => theme.colors.darkBlue};
 
-  &:hover, &:active {
-    background: ${({ theme }) => theme.colors.blue200} !important;
-  }
-
-  &:active {
-    border: 4px solid ${({ theme }) => theme.colors.blue300};
+  &:hover{
+    background: ${({ theme }) => theme.colors.blue200};
   }
 
   &:focus {
     background: ${({ theme }) => theme.colors.blue100};
+  }
+
+  &:active {
+    border: 4px solid ${({ theme }) => theme.colors.blue300};
+    background: ${({ theme }) => theme.colors.blue200};
   }
 `
 
